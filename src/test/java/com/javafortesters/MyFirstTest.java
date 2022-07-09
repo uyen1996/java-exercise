4477af6c6d9f1045b49017544294d37b6931dc75
Uyen <nntuyen1996@gmail.com>
2022-07-09 20:52:00 +0700

    first commit

---
+++ src/test/java/com/javafortesters/MyFirstTest.java
@@ -0,0 +1,20 @@
+package com.javafortesters;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+public class MyFirstTest {
+    @Test
+    public void canAddTwoPlusTwo() {
+        int answer = 2 + 2;
+        assertEquals("2+2=4", 4, answer);
+    }
+    @Test
+    public void canAddFourDivideTwo() {
+        int answer = 4 / 2;
+        assertEquals("4/2=2", 2, answer);
+    }
+    @Test
+    public void canAddTwoMultipleTwo() {
+        int answer = 2 * 2;
+        assertEquals("2*2 = 4", 4, answer);
+    }
+}
