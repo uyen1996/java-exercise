95f93b889c35f57a8aa5feddc2edccebbed369fa
Uyen <nntuyen1996@gmail.com>
2022-07-25 14:32:49 +0700

    updated

--- src/test/java/com/javafortesters/MyFirstTest.java
+++ src/test/java/com/javafortesters/MyFirstTest.java
@@ -1,14 +1,18 @@
 package com.javafortesters;
 import org.junit.Test;
+
+
+import static java.lang.Integer.toHexString;
 import static org.junit.Assert.assertEquals;
+
 public class MyFirstTest {
     @Test
-    public void canAddTwoPlusTwo() {
+    public void canAddTwoPlusTwo123() {
         int answer = 2 + 2;
         assertEquals("2+2=4", 4, answer);
     }
     @Test
-    public void canAddFourDivideTwo() {
+    public void canAddFourDivideTwo123() {
         int answer = 4 / 2;
         assertEquals("4/2=2", 2, answer);
     }
@@ -17,4 +21,9 @@ public class MyFirstTest {
         int answer = 2 * 2;
         assertEquals("2*2 = 4", 4, answer);
     }
+    @Test
+    public void canConvertIntToHex() {
+        Integer eleven = new Integer("11");
+        String hex = new String("b");
+        assertEquals("11 returns b", hex , toHexString(eleven));    }
 }
